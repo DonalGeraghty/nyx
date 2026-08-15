1c8144039cf3e3c1abc896329b815af2db96e381
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2026-08-15 10:49:35 +0100

    AmbientBackground

---
+++ src/components/AmbientBackground.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AmbientBackground() {
+  return (
+    <div className="nyx-ambient" aria-hidden="true">
+      <span className="nyx-ambient-orbit nyx-ambient-orbit-one" />
+      <span className="nyx-ambient-orbit nyx-ambient-orbit-two" />
+      <span className="nyx-ambient-grain" />
+    </div>
+  )
+}
+
+export default AmbientBackground
